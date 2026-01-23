e7d6b9687861d1952f5454c2810c0a32ff0732b5
blog-andi <kawankawanbaru@gmail.com>
2026-01-23 09:10:17 +0800

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,27 +3,6 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Tema',
-      links: [
-        {
-          text: 'Undangan Pernikahan',
-          href: getPermalink('/tema/undangan-pernikahan'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
-    },
-    {
       text: 'Pages',
       links: [
         {
